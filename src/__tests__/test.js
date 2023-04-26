a00257de9d4122044a135716e2fae6aa41fab59a
Squid90 <squid90@rambler.ru>
2023-04-26 21:41:53 +0300

    add test

--- src/__tests__/test.js
+++ src/__tests__/test.js
@@ -1,44 +1,44 @@
-import showFightingCard from '../game';
+import Validator from '../validator';
 
-const expected = [
-  {
-    id: 8,
-    name: 'Двойной выстрел',
-    description: 'Двойной выстрел наносит двойной урон',
-    icon: 'http://...',
-  },
-  {
-    id: 9,
-    name: 'Нокаутирующий удар',
-    description: 'Описание недоступно',
-    icon: 'http://...',
-  },
-];
+const expectedTrue = true;
+const expectedFalse = false;
 
 // eslint-disable-next-line
-test('Проверка вывода способностей в бою', () => {
-  const character = {
-    name: 'Лучник',
-    type: 'Bowman',
-    health: 50,
-    level: 3,
-    attack: 40,
-    defence: 10,
-    special: [
-      {
-        id: 8,
-        name: 'Двойной выстрел',
-        icon: 'http://...',
-        description: 'Двойной выстрел наносит двойной урон',
-      },
-      {
-        id: 9,
-        name: 'Нокаутирующий удар',
-        icon: 'http://...',
-      },
-    ],
-  };
-  const currentSpecial = showFightingCard(character);
+test('Проверка допустимого имени', () => {
+  const name = 'AS-D_z986xc';
+  const validName = new Validator(name);
   // eslint-disable-next-line
-  expect(expected).toEqual(currentSpecial);
+  expect(expectedTrue).toEqual(validName.validateUsername(name));
+});
+
+// eslint-disable-next-line
+test('Проверка НЕдопустимого имени. Символы в начале', () => {
+  const name = '-9AS-D_z986xc';
+  const validName = new Validator(name);
+  // eslint-disable-next-line
+  expect(expectedFalse).toEqual(validName.validateUsername(name));
+});
+
+// eslint-disable-next-line
+test('Проверка НЕдопустимого имени. Символы в конце', () => {
+  const name = 'AS-D_z986xc_5';
+  const validName = new Validator(name);
+  // eslint-disable-next-line
+  expect(expectedFalse).toEqual(validName.validateUsername(name));
+});
+
+// eslint-disable-next-line
+test('Проверка НЕдопустимого имени. Много цифр подряд', () => {
+  const name = 'AS-D_z9896xc';
+  const validName = new Validator(name);
+  // eslint-disable-next-line
+  expect(expectedFalse).toEqual(validName.validateUsername(name));
+});
+
+// eslint-disable-next-line
+test('Проверка НЕдопустимого имени. Кииллица', () => {
+  const name = 'AS-DАПР_z96xc';
+  const validName = new Validator(name);
+  // eslint-disable-next-line
+  expect(expectedFalse).toEqual(validName.validateUsername(name));
 });
--- src/validator.js
+++ src/validator.js
@@ -1,15 +1,14 @@
 export default class Validator {
   constructor(name) {
-    this.name = name
+    this.name = name;
   }
 
-  validateUsername(name) {
-    const accessSymbols = /^[A-Za-z0-9-_]+$/.test(name);
-    const startSymbols = /^[^-_0-9]/.test(name);
-    const endSymbols = /[^-_0-9]$/.test(name);
-    const repeatSymbols = !(/\d{4}/).test(name);
+  validateUsername() {
+    const accessSymbols = /^[A-Za-z0-9-_]+$/.test(this.name);
+    const startSymbols = /^[^-_0-9]/.test(this.name);
+    const endSymbols = /[^-_0-9]$/.test(this.name);
+    const repeatSymbols = !(/\d{4}/).test(this.name);
 
-    return accessSymbols && startSymbols && endSymbols && repeatSymbols
+    return accessSymbols && startSymbols && endSymbols && repeatSymbols;
   }
 }
-
